8c663a7b0c2de795270bcc22602b9681c81f7d82
薛建 <xuej612@163.com>
2016-07-22 20:24:59 +0800

    add

--- src/main/java/com/hongzhi/zswh/app_1_3/service/CheckClubTime.java
+++ src/main/java/com/hongzhi/zswh/app_1_3/service/CheckClubTime.java
@@ -35,7 +35,7 @@ public class CheckClubTime {
     @Autowired
     private V6ClubService v6ClubService;
 
-    @Scheduled(cron = "0 */5 * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     // @Scheduled(cron = "0 0 0/1 * * ?")
     public void checkClub() {
 
