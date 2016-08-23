b080d5a3d85df34a5c41b8cfb45d6d73458ebffd
薛建 <xuej612@163.com>
2016-08-23 15:01:20 +0800

    add 群组

--- src/main/java/com/hongzhi/zswh/app_1_4/entity/EventCreate.java
+++ src/main/java/com/hongzhi/zswh/app_1_4/entity/EventCreate.java
@@ -40,7 +40,7 @@ public class EventCreate {
     private String form_item;
 
     private String event_detail_rich_text;
-
+    private String group_id;
 
 
     private String dic_p_code = "event";
@@ -80,6 +80,13 @@ public class EventCreate {
     }
 
 
+    public String getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(String group_id) {
+        this.group_id = group_id;
+    }
 
     public Integer getEvent_id() {
         return event_id;
