031ac228771c24ecdddcb257ed1288ae7af7df59
薛建 <xuej612@163.com>
2016-08-23 16:12:06 +0800

    add sql脚本

--- databaseUpdate/update.sql
+++ databaseUpdate/update.sql
@@ -261,3 +261,17 @@ select
  ,mail_address
 from user_info
 where mail_address is not null ;
+
+
+
+DROP TABLE IF EXISTS `rest_user_info`;
+CREATE TABLE `rest_user_info` (
+  `user_id` int(11) DEFAULT NULL,
+  `user_login_name` varchar(100) DEFAULT NULL,
+  `rest_user_name` varchar(100) DEFAULT NULL,
+  `rest_user_password` varchar(100) DEFAULT NULL,
+  `is_delete` varchar(2) DEFAULT '0',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE event ADD  column  group_id varchar(100) DEFAULT '0';
