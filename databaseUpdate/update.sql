8e9e374364e3ab9865dad2321fba16b040d71521
薛建 <xuej612@163.com>
2016-08-31 15:32:52 +0800

    修改sql

--- databaseUpdate/update.sql
+++ databaseUpdate/update.sql
@@ -4,6 +4,7 @@ INSERT INTO `dictionary` (`code`, `value`, `p_code`, `language`) VALUES ('1091',
 INSERT INTO `dictionary` (`code`, `value`, `p_code`, `language`) VALUES ('1092', '报名人数已满', 'return_info', 'zh');
 UPDATE dictionary set value ='报名人数已满' where code = 'full' and p_code = 'event_button';
 
+DROP TABLE IF EXISTS `rest_user_info`;
 CREATE TABLE `rest_user_info` (
   `user_id` int(11) DEFAULT NULL,
   `user_login_name` varchar(100) DEFAULT NULL,
@@ -13,9 +14,8 @@ CREATE TABLE `rest_user_info` (
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE event ADD  column  group_id varchar(100) DEFAULT '0';
 
-
+DROP TABLE IF EXISTS `event_group`;
 CREATE TABLE `event_group` (
   `user_id` int(11) DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
